1f9e9619e68c3f236c8a3f38eaf3edb86e52c3d8
Andrew Milich <milichab@gmail.com>
2025-11-28 23:26:33 -0500

    rail

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,5 @@
 import nextConfig from "eslint-config-next";
 
-const config = [
-  ...nextConfig,
-  {
-    rules: {
-      // Disable immutability check for refs - mutating refs is valid
-      "react-hooks/immutability": "off",
-    },
-  },
-];
+const config = [...nextConfig];
 
 export default config;
